7cf10cd704cbc092e5d1762b4c647be1be47fdb1
Solfligh <xofligh28@gmail.com>
2026-02-11 05:31:07 -0600

    Update page.tsx

--- app/projects/fxco-pilot/page.tsx
+++ app/projects/fxco-pilot/page.tsx
@@ -1,7 +1,5 @@
 import { redirect } from "next/navigation";
 
-export const dynamic = "force-static";
-
 export default function FXCOPilotRedirectPage() {
   redirect("https://fxco-pilot.solflightech.org");
 }
